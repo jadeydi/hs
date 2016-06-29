95ca1083506c2fdfa4d5b01347e8b37b50950773
jadeydi <im.yuqlee@gmail.com>
2016-06-29 23:42:35 +0800

    Fix attachments don't show bug

--- api/routes/home.js
+++ api/routes/home.js
@@ -5,20 +5,26 @@ var factView = require('../views/fact');
 
 router.get('/', function(req, res) {
   var obj = {}
-  models.facts.findOne({order: 'random()'}).then(function(fact) {
+  models.facts.find({order: 'random()'}).then(function(fact) {
     if (fact == null) {
       res.status(404).json({});
     } else {
-      obj.current = factView.renderFact(fact);
-      models.facts.find({where: {id: {$lt: fact.id}}, order: 'id DESC'}).then(function(prev) {
-        if (prev != null) {
-          obj.prev = factView.renderFact(prev);
-        }
-        models.facts.find({where: {id: {$gt: fact.id}}}).then(function(next) {
-          if (next != null) {
-            obj.next = factView.renderFact(next);
+      models.attachments.findAll({where: {targetType: 'facts', targetId: fact.id}}).then(function(attachments) {
+        fact.attachments = attachments;
+        obj.current = factView.renderFact(fact);
+        models.facts.find({where: {id: {$lt: fact.id}}, order: 'id DESC', include: [{model: models.attachments}]}).then(function(prev) {
+          if (prev != null) {
+            obj.prev = factView.renderFact(prev);
           }
-          res.json(obj);
+          models.facts.find({where: {id: {$gt: fact.id}}, include: [{model: models.attachments}]}).then(function(next) {
+            if (next != null) {
+              obj.next = factView.renderFact(next);
+            }
+            res.json(obj);
+          })
+          .catch(function(error) {
+            res.status(500).json({});
+          });
         })
         .catch(function(error) {
           res.status(500).json({});
