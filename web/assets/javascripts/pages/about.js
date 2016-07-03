ba85cb22818261ae68c3029bef72511be8eb1582
jadeydi <im.yuqlee@gmail.com>
2016-07-03 22:35:12 +0800

    Update about

--- web/assets/javascripts/pages/about.js
+++ web/assets/javascripts/pages/about.js
@@ -8,10 +8,22 @@ const About = React.createClass({
           关于网站:
         </h3>
         <p>
-          用来收集游戏中的一些技巧，知识。
+          用来收集游戏(目前只有炉石传说)中的一些技巧，(冷)知识。这些内容来自各个角落，并非原创，侵删！
+        </p>
+        <h3>
+          关于我
+        </h3>
+        <p>
+          炉石ID: 不明真相的雷#5261, 中国区。非专业，任务，乱斗玩家。
         </p>
         <p>
-          如果你有相关的内容，非常欢迎提供给我 <a href='mailto:im.yuqlee@gmai.com'> im.yuqlee(at)gmail(dot)com </a>
+          如果你有相关的内容或者建议，请发邮件给我 <a href='mailto:im.yuqlee@gmai.com'> im.yuqlee(at)gmail(dot)com </a>
+        </p>
+        <h3>
+          QA
+        </h3>
+        <p>
+          现在还没遇到问题。
         </p>
       </div>
     )
