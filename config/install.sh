054a48c4ef413650c595c907934e90417da8314e
jadeydi <im.yuqlee@gmail.com>
2016-07-03 19:05:01 +0800

    Fix short key don't change browser histroy

--- config/install.sh
+++ config/install.sh
@@ -3,3 +3,4 @@ node_modules/sequelize-cli/bin/sequelize db:migrate
 # npm install -g pg
 #pm2 start bin/www
 pm2 restart bin/www
+# rsync -avvur web/public/javascripts/* cksixty:/var/www/repos/hs/web/public/javascripts
--- web/assets/javascripts/pages/fact.js
+++ web/assets/javascripts/pages/fact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import client from '../client';
 import variables from '!json!../../../../config/variables';
 import qiniu from '!json!../../../../config/qiniu';
-import { Link } from 'react-router';
+import { Link, withRouter } from 'react-router';
 
 const Image = React.createClass({
   render() {
@@ -17,114 +17,120 @@ const Image = React.createClass({
   },
 });
 
-const Fact = React.createClass({
-  getInitialState: function() {
-    return {current: {attachments: []}}
-  },
+const Fact = withRouter (
+  React.createClass({
+    getInitialState: function() {
+      return {current: {attachments: []}}
+    },
 
-  componentDidMount: function() {
-    var url = '/';
-    if (this.props.params.id != undefined) {
-      this.setState({current: {id: this.props.params.id, attachments: []}})
-      url = '/facts/' + this.props.params.id;
-    }
-    this.serverRequest = client(url).done(function(result) {
-      this.setState(result, function() {
-        if (!!this.state.prev) {
-          $('.js-fact-prev').show();
-        }
-        if (!!this.state.next) {
-          $('.js-fact-next').show();
-        }
-      });
-    }.bind(this));
+    componentDidMount: function() {
+      var url = '/';
+      if (this.props.params.id != undefined) {
+        this.setState({current: {id: this.props.params.id, attachments: []}})
+        url = '/facts/' + this.props.params.id;
+      }
+      this.serverRequest = client(url).done(function(result) {
+        this.setState(result, function() {
+          if (!!this.state.prev) {
+            $('.js-fact-prev').show();
+          }
+          if (!!this.state.next) {
+            $('.js-fact-next').show();
+          }
+        });
+      }.bind(this));
 
-    window.addEventListener('keydown', this.handleKeydown, true);
-  },
+      window.addEventListener('keydown', this.handleKeydown, true);
+    },
 
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
+    componentWillUnmount: function() {
+      this.serverRequest.abort();
+    },
 
-  render() {
-    var prevLink, nextLink;
-    if (!!this.state.prev) {
-      prevLink = <Link to={`/facts/${this.state.prev.id}`} className="fact prev js-fact-prev" onClick={this.prev}>&#8678;</Link>;
-    } else {
-      prevLink = <a href='javascript:;' className="fact next disable js-fact-left">&#8678;</a>;
-    }
+    render() {
+      var prevLink, nextLink;
+      if (!!this.state.prev) {
+        prevLink = <Link to={`/facts/${this.state.prev.id}`} className="fact prev js-fact-prev" onClick={this.prev}>&#8678;</Link>;
+      } else {
+        prevLink = <a href='javascript:;' className="fact next disable js-fact-left">&#8678;</a>;
+      }
 
-    if (!!this.state.next) {
-      nextLink = <Link to={`/facts/${this.state.next.id}`} className="fact next js-fact-next" onClick={this.next}>&#8680;</Link>;
-    } else {
-      nextLink = <a href='javascript:;' className="fact next disable js-fact-next">&#8680;</a>;
-    }
+      if (!!this.state.next) {
+        nextLink = <Link to={`/facts/${this.state.next.id}`} className="fact next js-fact-next" onClick={this.next}>&#8680;</Link>;
+      } else {
+        nextLink = <a href='javascript:;' className="fact next disable js-fact-next">&#8680;</a>;
+      }
 
-    return (
-      <div className='fact show'>
-        <h1 className='title'><img src={qiniu.domain + '/brands/hearthstone.png'} width='175px' height='65px'/></h1>
-        <h2 className='num'>
-          <span className='symbol'>#</span><span>{this.state.current.id}</span>
-        </h2>
-        <div>
-          {this.state.current.description}
-        </div>
-        <div className='images'>
-          {this.state.current.attachments.map(function(attachment) {
-            return <Image key={attachment.id} url={attachment.path}/>
-          })}
-        </div>
-        <div className='tags'>
-          标签: <span>{variables.heroes[this.state.current.hero]}</span>
-        </div>
-        <div className='action'>
-          {prevLink}
-          <span></span>
-          {nextLink}
-        </div>
-        <div className='hiden image-modal js-image-modal'>
-          <div className='close'>
-            <a href='javascript:;' onClick={this.hidenImageModal}>✕</a>
+      return (
+        <div className='fact show'>
+          <h1 className='title'><img src={qiniu.domain + '/brands/hearthstone.png'} width='175px' height='65px'/></h1>
+          <h2 className='num'>
+            <span className='symbol'>#</span><span>{this.state.current.id}</span>
+          </h2>
+          <div>
+            {this.state.current.description}
           </div>
-          <div className='image-container js-image-container'>
-            <img src="http://o9dhmjmlm.bkt.clouddn.com/attachments/a9adb472-53d7-4460-830e-281a72876729.png?imageView2/2/w/1000/h/700" />
+          <div className='images'>
+            {this.state.current.attachments.map(function(attachment) {
+              return <Image key={attachment.id} url={attachment.path}/>
+              })}
+            </div>
+            <div className='tags'>
+              标签: <span>{variables.heroes[this.state.current.hero]}</span>
+            </div>
+            <div className='action'>
+              {prevLink}
+              <span></span>
+              {nextLink}
+            </div>
+            <div className='hiden image-modal js-image-modal'>
+              <div className='close'>
+                <a href='javascript:;' onClick={this.hidenImageModal}>✕</a>
+              </div>
+              <div className='image-container js-image-container'>
+                <img src="http://o9dhmjmlm.bkt.clouddn.com/attachments/a9adb472-53d7-4460-830e-281a72876729.png?imageView2/2/w/1000/h/700" />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    )
-  },
+      )
+    },
 
-  prev: function() {
-    if (!this.state.prev) {
-      return
-    }
-    this.serverRequest = client('/facts/'+this.state.current.id+'/prev').done(function(result) {
-      var obj = {next: this.state.current, current: result[0], prev: result[1]}
-      this.setState(obj);
-    }.bind(this));
-  },
+    prev: function() {
+      if (!this.state.prev) {
+        return
+      }
+      this.serverRequest = client('/facts/'+this.state.current.id+'/prev').done(function(result) {
+        var obj = {next: this.state.current, current: result[0], prev: result[1]}
+        this.setState(obj, function() {
+          this.props.router.replace('/facts/'+this.state.current.id)
+        });
+      }.bind(this));
+    },
 
-  next: function() {
-    if (!this.state.next) {
-      return
-    }
-    this.serverRequest = client('/facts/'+this.state.current.id+'/next').done(function(result) {
-      var obj = {prev: this.state.current, current: result[0], next: result[1]}
-      this.setState(obj);
-    }.bind(this));
-  },
+    next: function() {
+      if (!this.state.next) {
+        return
+      }
+      this.serverRequest = client('/facts/'+this.state.current.id+'/next').done(function(result) {
+        var obj = {prev: this.state.current, current: result[0], next: result[1]}
+        this.setState(obj, function() {
+          this.props.router.replace('/facts/'+this.state.current.id)
+        });
+      }.bind(this));
+    },
 
-  hidenImageModal: function() {
-    $('.js-image-modal').hide();
-  },
+    hidenImageModal: function() {
+      $('.js-image-modal').hide();
+    },
 
-  handleKeydown: function(e) {
-    if (e.keyCode == 39 || e.keyCode == 74) {
-      this.next();
-    } else if (e.keyCode == 37 || e.keyCode == 75) {
-      this.prev();
-    }
-  },
-});
+    handleKeydown: function(e) {
+      if (e.keyCode == 39 || e.keyCode == 74) {
+        this.next();
+      } else if (e.keyCode == 37 || e.keyCode == 75) {
+        this.prev();
+      }
+    },
+  })
+);
 
 module.exports = {fact: Fact}
