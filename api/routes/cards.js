cf8b1bb8af07ddde67b4a53aa3efb5537b87c458
jadeydi <im.yuqlee@gmail.com>
2016-07-19 21:53:20 +0800

    Add co

--- api/routes/cards.js
+++ api/routes/cards.js
@@ -1,61 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var co = require('co');
 var models = require('../../models');
 var cardView = require('../views/card');
 var Sequelize = require('sequelize');
 
 router.get('/cards', function(req, res, next) {
-  models.cards.find({order: [[Sequelize.fn('RANDOM')]]}).then(function(card) {
+  co(function* () {
+    var card = yield models.cards.find({order: [[Sequelize.fn('RANDOM')]]});
     if (card == null) {
-      var error = new Error()
-      error.status = 404
-      next(error)
+      res.status(404).json({})
     } else {
-      obj = {current: cardView.renderCard(card)};
-      models.cards.find({where: {id: {$lt: card.id}}, order: [['id', 'DESC']]}).then(function(prev) {
-        if (prev != null) {
-          obj.prev = cardView.renderCard(prev);
-        }
-        models.cards.find({where: {id: {$gt: card.id}}, order: [['id', 'ASC']]}).then(function(next) {
-          obj.next = cardView.renderCard(next)
-          res.json(obj)
-        }).catch(function(error) {
-          next(error);
-        })
-      }).catch(function(error) {
-        next(error);
-      });
+      var obj = {current: cardView.renderCard(card)};
+      var prev = models.cards.find({where: {id: {$lt: card.id}}, order: [['id', 'DESC']]});
+      var next = models.cards.find({where: {id: {$gt: card.id}}, order: [['id', 'ASC']]})
+      var others = yield {prev: prev, next: next};
+
+      if (others.prev != null) {
+        obj.prev = cardView.renderCard(prev);
+      }
+
+      if (others.next != null) {
+        obj.next = cardView.renderCard(next)
+      }
+
+      res.json(obj);
     }
   }).catch(function(error) {
     next(error);
-  });
+  })
 });
 
 router.get('/cards/:id', function(req, res, next) {
-  models.cards.findById(req.params.id).then(function(card) {
+  co(function* () {
+    var card = yield models.cards.findById(req.params.id);
     if (card == null) {
-      var error = new Error()
-      error.status = 404
-      next(error)
+      res.status(404).json({})
     } else {
-      obj = {current: cardView.renderCard(card)};
-      models.cards.find({where: {id: {$lt: card.id}}, order: [['id', 'DESC']]}).then(function(prev) {
-        if (prev != null) {
-          obj.prev = cardView.renderCard(prev);
-        }
-        models.cards.find({where: {id: {$gt: card.id}}, order: [['id', 'ASC']]}).then(function(next) {
-          obj.next = cardView.renderCard(next)
-          res.json(obj)
-        }).catch(function(error) {
-          next(error);
-        })
-      }).catch(function(error) {
-        next(error);
-      });
+      var obj = {current: cardView.renderCard(card)};
+      var prev = models.cards.find({where: {id: {$lt: card.id}}, order: [['id', 'DESC']]});
+      var next = models.cards.find({where: {id: {$gt: card.id}}, order: [['id', 'ASC']]})
+      var others = yield {prev: prev, next: next};
+
+      if (others.prev != null) {
+        obj.prev = cardView.renderCard(prev);
+      }
+
+      if (others.next != null) {
+        obj.next = cardView.renderCard(next)
+      }
+
+      res.json(obj);
     }
   }).catch(function(error) {
     next(error);
-  });
+  })
 });
 
 router.get('/cards/:id/prev', function(req, res, next) {
--- api/routes/home.js
+++ api/routes/home.js
@@ -1,38 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var co = require('co');
 var models = require('../../models');
 var factView = require('../views/fact');
 var Sequelize = require('sequelize');
 
 router.get('/', function(req, res, next) {
-  var obj = {}
-  models.facts.find({order: [[Sequelize.fn('RANDOM')]], include: [{model: models.attachments}]}).then(function(fact) {
+  co(function* () {
+    var fact = yield models.facts.find({order: [[Sequelize.fn('RANDOM')]], include: [{model: models.attachments}]});
     if (fact == null) {
-      res.status(404).json({});
+      var error = new Error('');
+      error.status = 404;
+      yield Promise.reject(error);
     } else {
-      obj.current = factView.renderFact(fact);
-      models.facts.find({where: {id: {$lt: fact.id}}, order: [['id', 'DESC']], include: [{model: models.attachments}]}).then(function(prev) {
-        if (prev != null) {
-          obj.prev = factView.renderFact(prev);
-        }
-        models.facts.find({where: {id: {$gt: fact.id}}, order: [['id', 'ASC']], include: [{model: models.attachments}]}).then(function(next) {
-          if (next != null) {
-            obj.next = factView.renderFact(next);
-          }
-          res.json(obj);
-        })
-        .catch(function(error) {
-          next(error);
-        });
-      })
-      .catch(function(error) {
-        next(error);
-      });
+      var obj = {current: factView.renderFact(fact)};
+
+      var prev = models.facts.find({where: {id: {$lt: fact.id}}, order: [['id', 'DESC']], include: [{model: models.attachments}]});
+      var next = models.facts.find({where: {id: {$gt: fact.id}}, order: [['id', 'ASC']], include: [{model: models.attachments}]});
+      var other = yield {prev: prev, next: next}
+
+      if (other.prev != null) {
+        obj.prev = factView.renderFact(prev);
+      }
+
+      if (other.next != null) {
+        obj.next = factView.renderFact(next);
+      }
+
+      res.json(obj);
     }
-  })
-  .catch(function(error) {
+  }).catch(function(error) {
     next(error);
-  });
+  })
 });
 
 module.exports = router;
